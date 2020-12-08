1234656adca9161ccebaee3f6506288a65d717aa
camilled671 <72838912+camilled671@users.noreply.github.com>
2020-12-08 10:59:46 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+while True:
+    print("light level:"+input.light_level())
+    if input.light_level() > 0:
+        light.set_all(light.rgb(0,0,0))
+    else:
+        light.set_all(light.rgb(0,0,255))
