f519dda9281c0f082bd7a9c88d02b2b053a09c6a
camilled671 <72838912+camilled671@users.noreply.github.com>
2021-01-05 20:31:01 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-#while True:
-    #print("light level:"+input.light_level())
-    #if input.light_level() > 20:
-        #light.set_all(light.rgb(0,0,0))
-    #else:
-        #light.set_all(light.rgb(0,0,255))
 while True:
     print("light level:"+input.light_level())
-    if input.light_level() > 6:
+    if input.light_level() > 20:
         light.set_all(light.rgb(0,0,0))
-    elif input.light_level() <=13:
-        light.set_all(light.rgb(255,165,0))
     else:
-        input.light_level() <6
-        light.set_all(light.rgb(0,0,255))
+        light.set_all(light.rgb(255,0,0))
+#while True:
+   # print("light level:"+input.light_level())
+    #if input.light_level() > 6:
+        #light.set_all(light.rgb(0,0,0))
+    #elif input.light_level() <=13:
+       # light.set_all(light.rgb(255,165,0))
+    #else:
+        #input.light_level() <6
+        #light.set_all(light.rgb(0,0,255))
--- main.ts
+++ main.ts
@@ -1,18 +1,9 @@
-// while True:
-// print("light level:"+input.light_level())
-// if input.light_level() > 20:
-// light.set_all(light.rgb(0,0,0))
-// else:
-// light.set_all(light.rgb(0,0,255))
 while (true) {
     console.log("light level:" + input.lightLevel())
-    if (input.lightLevel() > 6) {
+    if (input.lightLevel() > 20) {
         light.setAll(light.rgb(0, 0, 0))
-    } else if (input.lightLevel() <= 13) {
-        light.setAll(light.rgb(255, 165, 0))
     } else {
-        input.lightLevel() < 6
-        light.setAll(light.rgb(0, 0, 255))
+        light.setAll(light.rgb(255, 0, 0))
     }
     
 }
